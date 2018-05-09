d63e8cf96da83a9c2fe69be0294563b9645e3c6b
OhYongTaek <ka123ak1@gmail.com>
2018-05-09 20:48:14 +0900

    [5/9] 프로젝트 생성

---
+++ src/main/java/jaram/groupware/groupware/GroupwareApplication.java
@@ -0,0 +1,12 @@
+package jaram.groupware.groupware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupwareApplication.class, args);
+	}
+}
---
+++ src/test/java/jaram/groupware/groupware/GroupwareApplicationTests.java
@@ -0,0 +1,16 @@
+package jaram.groupware.groupware;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroupwareApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
