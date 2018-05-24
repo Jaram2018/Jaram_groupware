804da252c4b178b4ae0e5fa99f413cff73e138a6
Sooyong Shin <shinsy1023@gmail.com>
2018-05-24 21:01:38 +0900

    [05/24] addMember 추가

---
+++ src/main/java/jaram/groupware/groupware/application/MemberController.java
@@ -0,0 +1,51 @@
+package jaram.groupware.groupware.application;
+
+import jaram.groupware.groupware.model.Member;
+import jaram.groupware.groupware.model.value.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by NamHyunsil on 2018. 5. 15..
+ */
+
+@Controller
+public class MemberController {
+    @Autowired
+    Member member;
+    private List<Member> members = member.getMembers();
+
+    
+    @RequestMapping(path = "/mamber", method = RequestMethod.POST)
+    public String addMember(Map<String, Object> model, HttpServletRequest request){
+        model.put("isError", false);
+
+        String cardinalNumber = request.getParameter("cardinalNumber");
+        String name = request.getParameter("name");
+        String phone = request.getParameter("phone");
+        String email = request.getParameter("email");
+
+        if (cardinalNumber == null || name == null || phone == null || email == null ||
+                !member.checkIntegrity(new Email(email))){
+            model.put("isError", "true");
+            model.put("members", members);
+
+            return "lookUPMembers";
+        }
+
+        Member newMember = new Member(Integer.parseInt(cardinalNumber), name, phone, email);
+        member.addMember(newMember);
+        members = member.getMembers();
+
+        model.put("members", members);
+
+        return "lookUPMembers";
+    }
+}
