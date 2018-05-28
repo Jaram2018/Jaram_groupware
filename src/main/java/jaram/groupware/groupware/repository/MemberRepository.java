8a74cbe6c773fe9afbf4a5117c6bfe81bf2772e3
NamHyunSil <hsilnam3@gmail.com>
2018-05-28 20:46:18 +0900

    [추가] MemberRepository추가

---
+++ src/main/java/jaram/groupware/groupware/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package jaram.groupware.groupware.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by NamHyunsil on 2018. 5. 17..
+ */
+@Repository
+public interface MemberRepository{
+}
