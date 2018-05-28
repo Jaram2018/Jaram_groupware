244e1f3b5331834cba8108b9d96bc798c3e00706
NamHyunSil <hsilnam3@gmail.com>
2018-05-28 20:43:37 +0900

    [추가] generatedValue에 auto / @NotNull [삭제] ManyToOne으로 이어져 있던 AccountEvent를 삭제 [수정] method public -> protected 로 수정 <- AccountEventHistory를 숨기기 위하여

--- src/main/java/jaram/groupware/groupware/persistent/AccountEventHistory.java
+++ src/main/java/jaram/groupware/groupware/persistent/AccountEventHistory.java
@@ -1,6 +1,7 @@
 package jaram.groupware.groupware.persistent;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 /**
@@ -12,16 +13,52 @@ import java.util.Date;
 public class AccountEventHistory {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-
-    @JoinColumn
-    @ManyToOne
-    private AccountEvent accountEvent;
 
     @Column
     private boolean isCollect;
 
     @Column
     private Date collectDate;
+
+    @Column
+    @NotNull
+    private String email;
+
+    protected AccountEventHistory(){
+
+    }
+    protected AccountEventHistory(String email){
+        this.isCollect = false;
+        this.email = email;
+    }
+
+    protected long getId() {
+        return id;
+    }
+
+    protected Date getCollectDate() {
+        return collectDate;
+    }
+
+    protected void setCollectDate(Date collectDate) {
+        this.collectDate = collectDate;
+    }
+
+    protected String getEmail() {
+        return email;
+    }
+
+    protected void setEmail(String email) {
+        this.email = email;
+    }
+
+    protected boolean isCollect() {
+        return isCollect;
+    }
+
+    protected void setCollect(boolean collect) {
+        isCollect = collect;
+    }
 }
